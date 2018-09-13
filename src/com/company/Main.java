096cb4bc3334939c97417216b87509373a886531
emgamble <eg913@pvsd.org>
2018-09-13 19:24:36 -0400

    Still not sure what to say in these

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner numReader = new Scanner(System.in);
+        System.out.print("Enter an Integer: ");
+        int a = numReader.nextInt();
+        if (a % 2 == 0)
+            System.out.println("The Integer " + a + " is even");
+        else
+            System.out.println("The Integer " + a + " is odd");
+
+
+
+
+
     }
 }
